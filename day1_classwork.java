dca3b0bcad3d998c7199fe02a6975b639faa7842
Hanan Alrabieah <hamad.hn.ra@gmail.com>
2022-09-05 19:40:03 +0300

    Done!

---
+++ day1_classwork.java
@@ -0,0 +1,18 @@
+/* Online Java Compiler and Editor */
+public class HelloWorld{
+
+     public static void main(String []args){
+        //Activity 1
+        int age = 17;
+        String name = "Mohammed";
+        System.out.println("Hello, My name is " + name + " and I am " + age + " years old");
+        
+        //Activity 2
+        int height = 164;
+        if (height >= 160){
+             System.out.println("You can enter the game");
+        } else {
+             System.out.println("You can't enter the game");
+        }
+    }
+}
